3516aa8bf5ad0b9334be1df4b9b5805f6455529f
Anirudh Gangadharan <133327434+anirudhgangadharan@users.noreply.github.com>
2025-11-23 09:26:43 +0530

    feat: implement membrane leakage dynamics (LIF model) to simulate temporal decay
    Added resting potential and decay factor to Neuron class. Updated constructor and added update_time_step method for potential decay simulation.

--- neuron_primitive.cpp
+++ neuron_primitive.cpp
@@ -6,13 +6,23 @@ class Neuron {
     public:
         float potential;
         float threshold;
+        float resting_potential;
+        float decay_factor; //time constant
         
         Neuron() {
             potential = -70.0;
             threshold = -55.0;
-            cout << "Neuron created. Resting potential : -70mV" << endl;
+            resting_potential = -70.0;
+            decay_factor = 0.90; // Retain 90% of charge per time step (Leak 10%)
+            cout << "LIF Neuron created. Decay factor (Alpha)" << decay_factor << endl;
         }
         
+        void update_time_step() {
+            // The equation: V_new = V_old * Decay + V_rest * (1-Decay)
+            potential = (potential * decay factor) + (resting_potential * (1.0 - decay_factor));
+            cout << "...Time passes potential decays to:" << potential << "mV" << endl;
+            
+        }
         void input(float signal) {
             potential = potential + signal ;
             cout << "Received:" << signal << "mV | New potential :" << potential << "mV" <<endl;
@@ -28,10 +38,18 @@ class Neuron {
 int main() {
     Neuron n;
     
-    cout << "\n--- STIMULATION ---" << endl;
+    cout << "\n--- TEMPORAL SUMMATION ---" << endl;
     // Stimulus 1: Weak (sub-threshold)
-    n.input(5.0);
-    //Stimulus 2: Strong (Summation --> FIRE)
-    n.input(15.0);
+    n.input(10.0); //Goes to -60
+    
+    //Time passes (The leak)
+    n.update_time_step(); //should drop back to -70
+    
+    //Stimulus 2: 
+    n.input(10.0); //if leaked, this might not fire yet
+    
+    //Stimulus 3 (strong)
+    n.input(20.0)
+    
     return 0;
 }
